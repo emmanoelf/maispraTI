0b3e2f9c54ba9451275fc868f612551c01f01a22
Emmanoel <emmanoel_95@hotmail.com>
2024-05-20 11:53:27 -0300

    feat(HotelGuests): add checkin and checkout for guests

---
+++ modulo-01/lista-02/exercise-50/HotelGuest.js
@@ -0,0 +1,30 @@
+const { hotel_guests } = require('./utils/data');
+
+class HotelGuest{
+    constructor(id_hotel, name_client){
+        this.id_hotel = id_hotel;
+        this.name_client = name_client;
+        this.check_in = new Date().toLocaleString();
+        this.check_out = null;
+    }
+
+    toCheckIn(hotel_guest){
+        hotel_guests.push(hotel_guest);
+    }
+
+    toCheckOut(hotel_guest){
+        hotel_guest.check_out = new Date().toLocaleString();
+    }
+
+    findByNameClient(name_client){
+        const name_client_lower_case = name_client.toLowerCase();
+        const hotel_guest = hotel_guests.find((hotel_guest) => hotel_guest.name_client.toLowerCase() === name_client_lower_case);
+        return hotel_guest;
+    }
+
+    findAll(){
+        return hotel_guests;
+    }
+}
+
+module.exports = HotelGuest;
