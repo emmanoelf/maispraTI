f5adf8f5d48f229754e112951b0865d273765bde
Emmanoel <emmanoel_95@hotmail.com>
2024-05-20 13:11:04 -0300

    refactor(BookingService/HotelService): add try-catch

--- modulo-01/lista-02/exercise-50/services/BookingService.js
+++ modulo-01/lista-02/exercise-50/services/BookingService.js
@@ -3,30 +3,40 @@ const Hotel = require('../Hotel');
 
 class BookingService{
     addBooking(booking){
-        const { id_hotel } = booking;
-        const hotel = Hotel.prototype.findById(id_hotel);
-
-        if(!hotel){
-            throw new Error('Hotel não encontrado.');
-        }
-
-        if(hotel.available_rooms <= 0){
-            throw new Error('Desculpe! Não há mais quartos disponíveis neste hotel.');
-        }
-
-        Booking.prototype.addBooking(booking);
-        hotel.booked_rooms++;
+        try{
+            const { id_hotel } = booking;
+            const hotel = Hotel.prototype.findById(id_hotel);
+    
+            if(!hotel){
+                throw new Error('Hotel não encontrado.');
+            }
+    
+            if(hotel.available_rooms <= 0){
+                throw new Error('Desculpe! Não há mais quartos disponíveis neste hotel.');
+            }
+    
+            Booking.prototype.addBooking(booking);
+            hotel.booked_rooms++;
+            return "Reserva realizada com sucesso!";
+        }catch(error){
+            console.log(error.message);
+        }      
     }
 
     removeBooking(name_client){
-        const remove = Booking.prototype.removeBooking(name_client);
+        try{
+            const remove = Booking.prototype.removeBooking(name_client);
 
-        if(remove === -1){
-            throw new Error(`A reserva no nome de ${name_client} não foi encontrada!`);
-        }
-
-        const hotel = Hotel.prototype.findById(remove.id_hotel);
-        hotel.booked_rooms++;
+            if(remove === -1){
+                throw new Error(`A reserva no nome de ${name_client} não foi encontrada!`);
+            }
+    
+            const hotel = Hotel.prototype.findById(remove.id_hotel);
+            hotel.booked_rooms++;
+            return "Reserva cancelada com sucesso!";
+        }catch(error){
+            console.log(error.messsage);
+        }        
     }
 
     showAllBookings(){
--- modulo-01/lista-02/exercise-50/services/HotelService.js
+++ modulo-01/lista-02/exercise-50/services/HotelService.js
@@ -5,105 +5,137 @@ const isEmpty = require('../utils/emptyString');
 
 class HotelService{
     addHotel(hotel){
-        const { name, city, total_rooms } = hotel;
-        if(isEmpty(name) || isEmpty(city)){
-            throw new Error('Dados inválidos. Por favor insira-os corretamente.');
+        try{
+            const { name, city, total_rooms } = hotel;
+            if(isEmpty(name) || isEmpty(city)){
+                throw new Error('Dados inválidos. Por favor insira-os corretamente.');
+            }
+            
+            if(isNaN(total_rooms)){
+                throw new Error('Dados inválidos. Por favor insira-os corretamente.');
+            }
+            
+            Hotel.prototype.addHotel(hotel);
+            return "Hotel adicionado com sucesso";
+        }catch(error){
+            console.log(error.message);
         }
-        
-        if(isNaN(total_rooms)){
-            throw new Error('Dados inválidos. Por favor insira-os corretamente.');
-        }
-        
-        Hotel.prototype.addHotel(hotel);
     }
 
     findById(id){
-        const hotel = Hotel.prototype.findById(id);
+        try{
+            const hotel = Hotel.prototype.findById(id);
         
-        if(!hotel){
-            throw new Error('Hotel não encontrado.');
+            if(!hotel){
+                throw new Error('Hotel não encontrado.');
+            }
+            return hotel;
+        }catch(error){
+            console.log(error.message);
         }
-        return hotel;
     }
 
     findByCity(city){
-        const hotels = Hotel.prototype.findByCity(city);
+        try{
+            const hotels = Hotel.prototype.findByCity(city);
         
-        if(!hotels){
-            throw new Error('Hotel não encontrado.');
+            if(!hotels){
+                throw new Error('Hotel não encontrado.');
+            }
+
+            const mapped_hotels = hotels.map(hotel => {
+                if(hotel.ratings){
+                    let stringify = JSON.stringify(hotel.ratings);
+                    let remove_characters = stringify.replace(/\\"/g, '');
+                    hotel.ratings = remove_characters;
+                }
+                return {
+                    ...hotel,
+                    ratings: hotel.ratings ? hotel.ratings : "Sem avaliações"
+                };
+        });
+
+            return mapped_hotels;
+        }catch(error){
+            console.log(error.message);
         }
-
-        const mapped_hotels = hotels.map(hotel => {
-           if(hotel.ratings){
-            let stringify = JSON.stringify(hotel.ratings);
-            let remove_characters = stringify.replace(/\\"/g, '');
-            hotel.ratings = remove_characters;
-           }
-            return {
-                ...hotel,
-                ratings: hotel.ratings ? hotel.ratings : "Sem avaliações"
-            };
-        })
-
-        return mapped_hotels;
     }
 
     checkIn(name_client){
-        const search_booking = Booking.prototype.findByNameClient(name_client);
+        try{
+            const search_booking = Booking.prototype.findByNameClient(name_client);
 
-        if(!search_booking){
-            throw new Error(`Reserva no nome de ${name_client} não foi encontrada.`);
+            if(!search_booking){
+                throw new Error(`Reserva no nome de ${name_client} não foi encontrada.`);
+            }
+    
+            Hotel.prototype.checkIn(search_booking.id_hotel);
+            const save_hotel_guest = new HotelGuest(search_booking.id_hotel, name_client);
+            HotelGuest.prototype.toCheckIn(save_hotel_guest);
+            return "Check-in realizado com sucesso!";
+        }catch(error){
+            console.log(error.message);
         }
-
-        Hotel.prototype.checkIn(search_booking.id_hotel);
-        const save_hotel_guest = new HotelGuest(search_booking.id_hotel, name_client);
-        HotelGuest.prototype.toCheckIn(save_hotel_guest);
     }
 
     checkOut(name_client){
-        const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
+        try{
+            const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
 
-        if(!hotel_guest){
-            throw new Error(`O hospede ${name_client} não foi encontrado.`);
+            if(!hotel_guest){
+                throw new Error(`O hospede ${name_client} não foi encontrado.`);
+            }
+    
+            if(hotel_guest.check_out !== null){
+                throw new Error(`O hóspede ${name_client} já realizou o check-out.`);
+            }
+    
+            Hotel.prototype.checkOut(hotel_guest.id_hotel);
+            HotelGuest.prototype.toCheckOut(hotel_guest);
+            return "Check-out realizado com sucesso";
+        }catch(error){
+            console.log(error.messsage);
         }
-
-        if(hotel_guest.check_out !== null){
-            throw new Error(`O hóspede ${name_client} já realizou o check-out.`);
-        }
-
-        Hotel.prototype.checkOut(hotel_guest.id_hotel);
-        HotelGuest.prototype.toCheckOut(hotel_guest);
     }
 
     showOccupations(id){
-        const hotel = this.findById(id);
+        try{
+            const hotel = this.findById(id);
 
-        if(!hotel){
-            throw new Error('Hotel não encontrado.');
-        }
-
-        const guests = HotelGuest.prototype.findById(id);
-        return guests;
+            if(!hotel){
+                throw new Error('Hotel não encontrado.');
+            }
+    
+            const guests = HotelGuest.prototype.findById(id);
+            return guests;
+        }catch(error){
+            console.log(error.message);
+        }        
     }
 
     rateHotel(id, name_client, rate, comment){
-        const hotel = Hotel.prototype.findById(id);
-        const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
-
-        if(hotel.id !== hotel_guest.id_hotel){
-            throw new Error('O hóspede não esteve hospedado nesse hotel.');
-        }
-
-        if(hotel_guest.check_out === null){
-            throw new Error('O hospede só poderá realizar avaliação após sua estadia!');
-        }
-
-        if(!hotel.hasOwnProperty('ratings')){
-            hotel.ratings = [];
-        }
-
-        const rate_client = { client: name_client, rate, comment };
-        hotel.ratings.push(rate_client);
+        try{
+            const hotel = Hotel.prototype.findById(id);
+            const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
+    
+            if(hotel.id !== hotel_guest.id_hotel){
+                throw new Error('O hóspede não esteve hospedado nesse hotel.');
+            }
+    
+            if(hotel_guest.check_out === null){
+                throw new Error('O hospede só poderá realizar avaliação após sua estadia!');
+            }
+    
+            if(!hotel.hasOwnProperty('ratings')){
+                hotel.ratings = [];
+            }
+    
+            const rate_client = { client: name_client, rate, comment };
+            hotel.ratings.push(rate_client);
+            return "Avaliação realizada com sucesso!";
+        }catch(error){
+            console.log(error.message);
+        }        
     }
 }
 
