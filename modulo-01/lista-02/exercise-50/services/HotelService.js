c6602907daccf741584e179531c90501b9e77ed3
Emmanoel <emmanoel_95@hotmail.com>
2024-05-20 11:57:19 -0300

    feat(HotelService): add checkin, checkout, occupations and rate hotel methods

--- modulo-01/lista-02/exercise-50/services/HotelService.js
+++ modulo-01/lista-02/exercise-50/services/HotelService.js
@@ -1,17 +1,20 @@
 const Hotel = require('../Hotel');
+const HotelGuest = require('../HotelGuest');
+const Booking = require('../Booking');
+const isEmpty = require('../utils/emptyString');
 
 class HotelService{
     addHotel(hotel){
         const { name, city, total_rooms } = hotel;
-        if(name === undefined || city === undefined){
+        if(isEmpty(name) || isEmpty(city)){
             throw new Error('Dados inválidos. Por favor insira-os corretamente.');
         }
-    
-        if(total_rooms === isNaN){
+        
+        if(isNaN(total_rooms)){
             throw new Error('Dados inválidos. Por favor insira-os corretamente.');
         }
-    
-        Hotel.prototype.addHotel(hotel);        
+        
+        Hotel.prototype.addHotel(hotel);
     }
 
     findById(id){
@@ -20,18 +23,87 @@ class HotelService{
         if(!hotel){
             throw new Error('Hotel não encontrado.');
         }
-
         return hotel;
     }
 
     findByCity(city){
-        const hotel = Hotel.prototype.findByCity(city);
+        const hotels = Hotel.prototype.findByCity(city);
         
+        if(!hotels){
+            throw new Error('Hotel não encontrado.');
+        }
+
+        const mapped_hotels = hotels.map(hotel => {
+           if(hotel.ratings){
+            let stringify = JSON.stringify(hotel.ratings);
+            let remove_characters = stringify.replace(/\\"/g, '');
+            hotel.ratings = remove_characters;
+           }
+            return {
+                ...hotel,
+                ratings: hotel.ratings ? hotel.ratings : "Sem avaliações"
+            };
+        })
+
+        return mapped_hotels;
+    }
+
+    checkIn(name_client){
+        const search_booking = Booking.prototype.findByNameClient(name_client);
+
+        if(!search_booking){
+            throw new Error(`Reserva no nome de ${name_client} não foi encontrada.`);
+        }
+
+        Hotel.prototype.checkIn(search_booking.id_hotel);
+        const save_hotel_guest = new HotelGuest(search_booking.id_hotel, name_client);
+        HotelGuest.prototype.toCheckIn(save_hotel_guest);
+    }
+
+    checkOut(name_client){
+        const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
+
+        if(!hotel_guest){
+            throw new Error(`O hospede ${name_client} não foi encontrado.`);
+        }
+
+        if(hotel_guest.check_out !== null){
+            throw new Error(`O hóspede ${name_client} já realizou o check-out.`);
+        }
+
+        Hotel.prototype.checkOut(hotel_guest.id_hotel);
+        HotelGuest.prototype.toCheckOut(hotel_guest);
+    }
+
+    showOccupations(id){
+        const hotel = this.findById(id);
+
         if(!hotel){
             throw new Error('Hotel não encontrado.');
         }
 
-        return hotel;
+        const guests = HotelGuest.prototype.findAll()
+        return guests;
+    }
+
+    rateHotel(id, name_client, rate, comment){
+        const hotel = Hotel.prototype.findById(id);
+        const hotel_guest = HotelGuest.prototype.findByNameClient(name_client);
+
+        if(hotel.id !== hotel_guest.id_hotel){
+            throw new Error('O hóspede não esteve hospedado nesse hotel.');
+        }
+
+        if(hotel_guest.check_out === null){
+            throw new Error('O hospede só poderá realizar avaliação após sua estadia!');
+        }
+
+        if(!hotel.hasOwnProperty('ratings')){
+            hotel.ratings = [];
+        }
+
+        const rate_client = { client: name_client, rate, comment };
+        hotel.ratings.push(rate_client);
     }
 }
 
