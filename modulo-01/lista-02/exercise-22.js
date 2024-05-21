838f6d241f85be552c46d16fb71352b0ee533bba
Emmanoel <emmanoel_95@hotmail.com>
2024-05-21 15:05:33 -0300

    refator(exercise-22): accept data from prompt

--- modulo-01/lista-02/exercise-22.js
+++ modulo-01/lista-02/exercise-22.js
@@ -1,27 +1,38 @@
-const population = [
-    { salary: 4500.00, number_children: 2 },
-    { salary: 5700.00, number_children: 1 },
-    { salary: 2300.00, number_children: 1 },
-    { salary: 1350.00, number_children: 0 },
-    { salary: 350.00, number_children: 0 },
-    { salary: 250.00, number_children: 0 },
-];
+const prompt = require('prompt-sync')();
 
-function populationStatistics(population){
+function populationStatistics(){
+    let population = [];
+    let highest_salary = Number.NEGATIVE_INFINITY;
     let sum_salary = 0;
     let sum_number_children = 0;
-    let highest_salary = Number.NEGATIVE_INFINITY;
     let salary_low = 0;
+    let option;
 
-    population.forEach(citizen => {
-        sum_salary += citizen.salary;
-        sum_number_children += citizen.number_children;
+    do{
+        let salary = parseFloat(prompt("Digite o salário do(a) cidadão(ã): "));
 
-        if(citizen.salary > highest_salary){
-            highest_salary = citizen.salary;
+        while(isNaN(salary)){
+            salary = parseFloat(prompt("Valor inválido. Por favor, digite o salário do(a) cidadão(ã): "));
         }
 
-        if(citizen.salary <= 350){
+        let number_children = parseInt(prompt("Digite a quantidade de filhos: "));
+        while(isNaN(number_children)){
+            number_children = parseInt(prompt("Valor inválido. Por favor, digite a quantidade de filhos: "));
+        }
+
+        let citizen = { salary, number_children };
+        population.push(citizen);
+        option = parseInt(prompt("Digite 1 para adicionar pessoas na tabela ou 0 para sair: "));
+    }while(option !== 0);
+
+    population.forEach(element => {
+        sum_salary += element.salary;
+        sum_number_children += element.number_children;
+        
+        if (element.salary > highest_salary) {
+            highest_salary = element.salary;
+        }
+        if (element.salary <= 350) {
             salary_low++;
         }
     });
@@ -29,12 +40,11 @@ function populationStatistics(population){
     const average_salary = sum_salary / population.length;
     const average_number_children = sum_number_children / population.length;
     const percentage_salary_low = (salary_low / population.length) * 100;
-    
-    return { average_salary, average_number_children, highest_salary, percentage_salary_low }
+    return { average_salary, average_number_children, highest_salary, percentage_salary_low };
 }
 
 function main(){
-    const {average_salary, average_number_children, highest_salary, percentage_salary_low } = populationStatistics(population);
+    const {average_salary, average_number_children, highest_salary, percentage_salary_low } = populationStatistics();
     console.log("Média de salários: " + average_salary.toFixed(2) + "\n" + "Média de número de filhos: " + average_number_children.toFixed(2)  + "\n" +
     "Salário mais alto: " + highest_salary.toFixed(2)  + "\n" + "Percentual de pessoas com salário até R$ 350,00: " + percentage_salary_low.toFixed(2) + "%" );
 }
