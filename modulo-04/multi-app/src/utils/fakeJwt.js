b0ac0438e9c1feec4bb615491d2928997d7619fb
Emmanoel <emmanoel_95@hotmail.com>
2024-08-20 19:00:44 -0300

    feat: base structure for task 05

---
+++ modulo-04/multi-app/src/utils/fakeJwt.js
@@ -0,0 +1,23 @@
+export function createFakeJwt(username){
+    const token = btoa(JSON.stringify({
+        sub: username,
+        iat: Date.now() / 1000,
+        exp: (Date.now() / 1000) + 3600,
+    }))
+
+    return token;
+}
+
+export function decodeFakeJwt(token){
+    try{
+        const decoded = JSON.parse(atob(token));
+        if(decoded.exp > Date.now() / 1000){
+            console.log("Passou na validacao")
+            return decoded;
+        }
+        console.log("Expired token");
+        throw new Error("Expired token");
+    } catch(error){
+        console.log(error);
+    }
+}
