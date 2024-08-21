7faee343e4786a27aeb07b689fda475ac00800c6
Emmanoel <emmanoel_95@hotmail.com>
2024-08-21 17:39:04 -0300

    refactor(NavBar): transform into component

--- modulo-04/multi-app/src/App.jsx
+++ modulo-04/multi-app/src/App.jsx
@@ -10,16 +10,7 @@ import {
 } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import styled from "styled-components";
-import {
-  FaQrcode,
-  FaSearch,
-  FaTasks,
-  FaRegQuestionCircle,
-  FaGlobeAmericas,
-  FaNetworkWired,
-  FaBars,
-  FaArrowLeft,
-} from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 import QRCodeGenerator from "./components/QRCodeGenarator";
 import IPAddressFinder from "./components/IPAddressFinder";
 import MovieSearchEngine from "./components/MovieSearchEngine";
@@ -31,6 +22,7 @@ import Footer from "./components/Footer";
 import "./App.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { decodeFakeJwt } from "./utils/fakeJwt";
+import NavBar from "./components/Navbar";
 
 // Estiliza o contêiner principal do aplicativo.
 const AppContainer = styled.div`
@@ -65,58 +57,6 @@ const CarouselContainer = styled.div`
   background-color: #2c3e50;
   border-radius: 20px;
   padding: 20px;
-`;
-
-// Estiliza a barra de navegação.
-const NavBar = styled.div`
-  width: 240px;
-  background-color: #2c3e50;
-  color: white;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  box-shadow: 3px 0 15px rgba(0, 0, 0, 0.3);
-
-  @media (max-width: 768px) {
-    width: 100%;
-    height: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
-  }
-`;
-
-// Estiliza o botão de alternância da barra de navegação.
-const NavBarToggle = styled.div`
-  display: none;
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  cursor: pointer;
-  z-index: 1000;
-
-  @media (max-width: 768px) {
-    display: block;
-  }
-`;
-
-// Estiliza os links na barra de navegação.
-const StyledLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-  padding: 12px;
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  transition: background-color 0.3s, color 0.3s;
-
-  &:hover {
-    background-color: #34495e;
-    color: #ecf0f1;
-  }
 `;
 
 // Estiliza os itens individuais do carrossel.
@@ -191,7 +131,6 @@ const CustomCarousel = styled(Carousel)`
 const App = () => {
   // Cria estados para autenticação, visibilidade da barra de navegação, componente atual, e índice do carrossel.
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isNavBarOpen, setIsNavBarOpen] = useState(false);
   const [currentComponent, setCurrentComponent] = useState(null);
   const [carouselIndex, setCarouselIndex] = useState(0);
   const navigate = useNavigate(); // Hook para navegação.
@@ -222,11 +161,6 @@ const App = () => {
   const handleLogout = () => {
     setIsAuthenticated(false);
     navigate("/");
-  };
-
-  // Alterna a visibilidade da barra de navegação.
-  const toggleNavBar = () => {
-    setIsNavBarOpen(!isNavBarOpen);
   };
 
   // Função para definir o componente atual a ser exibido e atualizar o índice do carrossel.
@@ -263,52 +197,13 @@ const App = () => {
   // Renderiza o componente principal.
   return (
     <AppContainer>
-      <NavBarToggle onClick={toggleNavBar}>
-        <FaBars size={24} color="#2C3E50" />
-      </NavBarToggle>
       {!isAuthenticated ? (
         <MainContent>
           <Login onLogin={handleLogin} />
         </MainContent>
       ) : (
         <>
-          <NavBar isOpen={isNavBarOpen}>
-            <StyledLink onClick={() => handleAccess(0, "QRCodeGenerator")}>
-              <FaQrcode />
-              QR Code Generator
-            </StyledLink>
-            <StyledLink onClick={() => handleAccess(1, "IPAddressFinder")}>
-              <FaNetworkWired />
-              IP Address Finder
-            </StyledLink>
-            <StyledLink onClick={() => handleAccess(2, "MovieSearchEngine")}>
-              <FaSearch />
-              Movie Search
-            </StyledLink>
-            <StyledLink onClick={() => handleAccess(3, "TodoApp")}>
-              <FaTasks />
-              Todo App
-            </StyledLink>
-            <StyledLink onClick={() => handleAccess(4, "QuizApp")}>
-              <FaRegQuestionCircle />
-              Quiz App
-            </StyledLink>
-            <StyledLink onClick={() => handleAccess(5, "LanguageTranslator")}>
-              <FaGlobeAmericas />
-              Translator
-            </StyledLink>
-            <button
-              onClick={handleLogout}
-              style={{
-                marginTop: "20px",
-                color: "white",
-                backgroundColor: "transparent",
-                border: "none",
-              }}
-            >
-              Logout
-            </button>
-          </NavBar>
+          <NavBar handleAccess={handleAccess} handleLogout={handleLogout}/>
           <MainContent>
             {currentComponent ? (
               <>
