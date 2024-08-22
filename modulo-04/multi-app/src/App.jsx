24acde5d3a68a1406a29efcee5828598dcc18217
Emmanoel <emmanoel_95@hotmail.com>
2024-08-22 14:11:30 -0300

    refactor(CarouselMultiApp): turn into component

--- modulo-04/multi-app/src/App.jsx
+++ modulo-04/multi-app/src/App.jsx
@@ -8,7 +8,6 @@ import {
   useLocation,
   Link,
 } from "react-router-dom";
-import { Carousel } from "react-responsive-carousel";
 import styled from "styled-components";
 import { FaArrowLeft } from "react-icons/fa";
 import QRCodeGenerator from "./components/QRCodeGenarator";
@@ -23,6 +22,7 @@ import "./App.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { decodeFakeJwt } from "./utils/fakeJwt";
 import NavBar from "./components/Navbar";
+import CarouselMultiApp from "./components/CarouselMultiApp";
 
 // Estiliza o contêiner principal do aplicativo.
 const AppContainer = styled.div`
@@ -45,62 +45,6 @@ const MainContent = styled.div`
   overflow: hidden;
 `;
 
-// Estiliza o contêiner do carrossel.
-const CarouselContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 80%;
-  height: 70%;
-  margin: auto;
-  background-color: #2c3e50;
-  border-radius: 20px;
-  padding: 20px;
-`;
-
-// Estiliza os itens individuais do carrossel.
-const CarouselItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: linear-gradient(135deg, #ff7e5f, #feb47b);
-  padding: 40px;
-  border-radius: 15px;
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
-  text-align: center;
-  transition: transform 0.3s, box-shadow 0.3s;
-  height: 100%;
-  width: 100%;
-
-  &:hover {
-    transform: scale(1.05);
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
-  }
-
-  h2 {
-    margin-bottom: 20px;
-    font-size: 24px;
-    color: white;
-  }
-
-  button {
-    padding: 10px 20px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 10px;
-    font-size: 16px;
-
-    &:hover {
-      background-color: #0056b3;
-    }
-  }
-`;
-
 // Estiliza o botão de retorno.
 const ReturnButton = styled.button`
   padding: 10px 20px;
@@ -116,14 +60,6 @@ const ReturnButton = styled.button`
 
   &:hover {
     background-color: #0056b3;
-  }
-`;
-
-// Estiliza o carrossel personalizado.
-const CustomCarousel = styled(Carousel)`
-  width: 100%;
-  .carousel-status {
-    display: none;
   }
 `;
 
@@ -213,56 +149,7 @@ const App = () => {
                 </ReturnButton>
               </>
             ) : (
-              <CarouselContainer>
-                <CustomCarousel
-                  showArrows={true}
-                  infiniteLoop={true}
-                  autoPlay={true}
-                  interval={5000}
-                  selectedItem={carouselIndex}
-                  onChange={(index) => setCarouselIndex(index)}
-                >
-                  <CarouselItem>
-                    <h2>QR Code Generator</h2>
-                    <button onClick={() => handleAccess(0, "QRCodeGenerator")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>IP Address Finder</h2>
-                    <button onClick={() => handleAccess(1, "IPAddressFinder")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Movie Search Engine</h2>
-                    <button
-                      onClick={() => handleAccess(2, "MovieSearchEngine")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Todo App</h2>
-                    <button onClick={() => handleAccess(3, "TodoApp")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Quiz App</h2>
-                    <button onClick={() => handleAccess(4, "QuizApp")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Language Translator</h2>
-                    <button
-                      onClick={() => handleAccess(5, "LanguageTranslator")}
-                    >
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                </CustomCarousel>
-              </CarouselContainer>
+              <CarouselMultiApp carouselIndex={carouselIndex} handleAccess={handleAccess}/>
             )}
             <Footer>© 2024 Your Company | All rights reserved</Footer>
           </MainContent>
